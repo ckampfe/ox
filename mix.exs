38de62b7ab3193c45bb9bb118387a3aa6e05d96c
Clark Kampfe <clark.kampfe@gmail.com>
2017-12-21 21:17:14 -0600

    prep for hex

--- mix.exs
+++ mix.exs
@@ -2,28 +2,29 @@ defmodule Ox.Mixfile do
   use Mix.Project
 
   def project do
-    [
-      app: :ox,
-      version: "0.1.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
-    ]
+    [app: :ox,
+     version: "0.1.0",
+     elixir: "~> 1.5",
+     package: package(),
+     description: "An implementation of Okasaki data structures",
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [extra_applications: [:logger]]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      {:stream_data, "~> 0.3", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    [{:stream_data, "~> 0.3", only: :test},
+     {:ex_doc, "~> 0.14", only: :dev}
     ]
+  end
+
+  def package do
+    [maintainers: ["Clark Kampfe"],
+     licenses: ["MIT"],
+     links: %{"Github" => "https://github.com/ckampfe/ox"}]
   end
 end
